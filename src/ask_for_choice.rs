20e087aa9d221bdee81b8afe04b43ce827bf8ac0
ElSalvo96 <74384157+ElSalvo96@users.noreply.github.com>
2023-10-28 17:06:42 +0200

    perf: change from struc to enum implementations

--- src/ask_for_choice.rs
+++ src/ask_for_choice.rs
@@ -7,58 +7,63 @@ pub(crate) fn get_choice(items: &[&str]) -> Result<usize, String> {
         return Err("No items to choose from".to_string());
     }
 
-    let mut choice = 1;
+    let mut choice = 0;
     loop {
-        print!("\x1b[2J\x1b[H"); // Clear the screen
+        // print!("\x1b[2J\x1b[H"); // Clear the screen
         println!("Choose an option by navigating with arrow keys and pressing Enter:");
         for (i, item) in items.iter().enumerate() {
-            println!("{} {}. {}", if choice == i + 1 { "->" } else { "  " }, i + 1, item);
+            print!("{} {}. {}\n", if choice == i { "->" } else { "  " }, i, item);
         }
 
         let key = get_key();
 
-        if key.is_down {
-            choice += 1;
-            if choice > items.len() {
-                choice = 1;
+        match key {
+            GetKeyResult::IsDown => {
+                choice += 1;
+                if choice > items.len() {
+                    choice = 0;
+                }
             }
-        } else if key.is_up {
-            choice -= 1;
-            if choice < 1 {
-                choice = 3;
+            GetKeyResult::IsUp => {
+                choice -= 1;
+                if choice < 1 {
+                    choice = items.len() - 1;
+                }
             }
-        } else if key.is_enter {
-            return Ok(choice);
+            GetKeyResult::IsEnter => {
+                return Ok(choice);
+            }
         }
     }
 }
 
-struct GetKeyResult {
-    is_down: bool,
-    is_up: bool,
-    is_enter: bool,
+enum GetKeyResult {
+    IsDown,
+    IsUp,
+    IsEnter,
 }
 
 impl GetKeyResult {
     fn new(event: Event) -> Result<Self, String> {
-        let obj = GetKeyResult {
-            is_down: event == Event::Key(KeyEvent::from(KeyCode::Down)) ||
-            event == Event::Key(KeyEvent::from(KeyCode::Right)),
-            is_up: event == Event::Key(KeyEvent::from(KeyCode::Up)) ||
-            event == Event::Key(KeyEvent::from(KeyCode::Left)),
-            is_enter: event == Event::Key(KeyEvent::from(KeyCode::Enter)),
-        };
-        if obj.is_down || obj.is_up || obj.is_enter {
-            return Ok(obj);
+        match event {
+            | Event::Key(KeyEvent { code: KeyCode::Down, .. })
+            | Event::Key(KeyEvent { code: KeyCode::Right, .. }) => {
+                Ok(GetKeyResult::IsDown)
+            }
+            | Event::Key(KeyEvent { code: KeyCode::Up, .. })
+            | Event::Key(KeyEvent { code: KeyCode::Left, .. }) => {
+                Ok(GetKeyResult::IsUp)
+            }
+            Event::Key(KeyEvent { code: KeyCode::Enter, .. }) => { Ok(GetKeyResult::IsEnter) }
+            _ => { Err("Not a valid key".to_string()) }
         }
-        Err("Not a valid key".to_string())
     }
 }
 
 fn get_key() -> GetKeyResult {
     loop {
         // `poll()` waits for an `Event` for a given time period
-        if poll(Duration::from_millis(500)).unwrap() {
+        if poll(Duration::from_millis(0)).unwrap() {
             // It's guaranteed that the `read()` won't block when the `poll()`
             // function returns `true`
             let event = read().unwrap();
